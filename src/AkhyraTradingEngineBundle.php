132dfc6eb8331dd0b6982ff7ac543057d1e65c29
alexis <alexiscassan@gmail.com>
2024-09-20 17:21:58 +0200

    Initial commit

---
+++ src/AkhyraTradingEngineBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Akhyra\TradingEngine;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AkhyraTradingEngineBundle extends Bundle
+{
+
+}
---
+++ src/DependencyInjection/AkhyraTradingEngineExtension.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Akhyra\TradingEngine\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class AkhyraTradingEngineExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+}
